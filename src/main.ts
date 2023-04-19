fbdf26607164ca9907f4e422c12ad693ce17abbb
lewismd13 <72758390+lewismd13@users.noreply.github.com>
2023-04-19 15:13:40 -0500

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { chatPrivate, getClanId, print } from "kolmafia";
+import { Clan } from "libram";
+
+export function main(_sender: string, message: string, _channel: string): void {
+  print(`New message from ${_sender} in channel ${_channel} saying "${message}"`, "yellow");
+
+  if (getClanId() !== 90485) Clan.join("Bonus Adventures from Hell");
+
+  const bafh = Clan.get();
+
+  if (message.includes("whitelist bafh") && !_channel) {
+    const bafh = Clan.get();
+    bafh.addPlayerToWhitelist(_sender);
+    chatPrivate(_sender, "You've been whitelisted to bafh, enjoy hell!");
+    print(`${_sender} has been whitelisted to bafh`, "yellow");
+  } else if (!_channel) {
+    chatPrivate(
+      _sender,
+      `I'm sorry, I didn't understand that. Please say "whitelist bafh" if you want a wl`
+    );
+    print(`Unknown message from ${_sender}`, "red");
+  }
+}
